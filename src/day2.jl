ee03bc8b2789e0eb6043fd0460b5dd1a25b2d9f2
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-12-02 18:20:27 -0700

    Part 2 solution

--- src/day2.jl
+++ src/day2.jl
@@ -6,10 +6,27 @@ function day2()
 
   input = map(parseln, readlines("src/day2.input"))
 
-  decode(command::Val{:forward}, val) = [val, 0]
-  decode(command::Val{:down}, val) = [0, val]
-  decode(command::Val{:up}, val) = [0, -val]
+  #Part 1
+  decode(::Val{:forward}, val) = [val, 0]
+  decode(::Val{:down}, val) = [0, val]
+  decode(::Val{:up}, val) = [0, -val]
 
   ans = reduce(*, sum([decode(cmd, val) for (cmd, val) in input]))
   println(ans)
+
+  #Part 2
+  decode_aim(_, _) = 0
+  decode_aim(::Val{:down}, val) = val
+  decode_aim(::Val{:up}, val) = -val
+  
+  decode_forward(_, _) = 0
+  decode_forward(::Val{:forward}, val) = val
+
+  aim = accumulate(+, [decode_aim(command, val) for (command, val) in input])
+  horizontal_Δ = [decode_forward(command, val) for (command, val) in input]
+
+  final_pos = sum(horizontal_Δ)
+  final_depth = sum(horizontal_Δ .* aim)
+
+  println(final_pos * final_depth)
 end

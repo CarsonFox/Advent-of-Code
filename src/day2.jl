044677dca5a56b4732408cc41752a907b74c0e40
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-12-02 18:22:11 -0700

    Variable renaming

--- src/day2.jl
+++ src/day2.jl
@@ -1,29 +1,29 @@
 function day2()
   function parseln(line)
-    command, val = split(line)
-    Val(Symbol(command)), parse(Int, val)
+    cmd, x = split(line)
+    Val(Symbol(cmd)), parse(Int, x)
   end
 
   input = map(parseln, readlines("src/day2.input"))
 
   #Part 1
-  decode(::Val{:forward}, val) = [val, 0]
-  decode(::Val{:down}, val) = [0, val]
-  decode(::Val{:up}, val) = [0, -val]
+  decode(::Val{:forward}, x) = [x, 0]
+  decode(::Val{:down}, x) = [0, x]
+  decode(::Val{:up}, x) = [0, -x]
 
-  ans = reduce(*, sum([decode(cmd, val) for (cmd, val) in input]))
+  ans = reduce(*, sum([decode(cmd, x) for (cmd, x) in input]))
   println(ans)
 
   #Part 2
   decode_aim(_, _) = 0
-  decode_aim(::Val{:down}, val) = val
-  decode_aim(::Val{:up}, val) = -val
+  decode_aim(::Val{:down}, x) = x
+  decode_aim(::Val{:up}, x) = -x
   
   decode_forward(_, _) = 0
-  decode_forward(::Val{:forward}, val) = val
+  decode_forward(::Val{:forward}, x) = x
 
-  aim = accumulate(+, [decode_aim(command, val) for (command, val) in input])
-  horizontal_Δ = [decode_forward(command, val) for (command, val) in input]
+  aim = accumulate(+, [decode_aim(cmd, x) for (cmd, x) in input])
+  horizontal_Δ = [decode_forward(cmd, x) for (cmd, x) in input]
 
   final_pos = sum(horizontal_Δ)
   final_depth = sum(horizontal_Δ .* aim)

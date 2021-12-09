e0d6787bde5276640673fed2895d9209079b8670
Carson Fox <carson.fox@aggiemail.usu.edu>
2021-12-08 22:13:49 -0700

    Day 5 part 2

--- src/day5.jl
+++ src/day5.jl
@@ -1,23 +1,34 @@
 function day5()
   input = readlines("src/day5.input")
 
-  function parseline(line)
-    coord_pairs = split.(split(line, "->"), ",")
-    #One-based indices
-    offset = CartesianIndex(1, 1)
-    map(pair -> CartesianIndex(parse.(Int, pair)...) + offset, coord_pairs)
+  function parse_pair(line)
+    map.(pair -> parse.(Int, pair) .+ 1,
+         split.(split(line, "->"), ","))
   end
 
-  indices = parseline.(input)
+  function symmetric_range(x, y)
+    step = x == y ? 1 : sign(y - x)
+    x:step:y
+  end
 
+  function line((a, b))
+    CartesianIndex.(symmetric_range(a[1], b[1]), symmetric_range(a[2], b[2]))
+  end
+
+  pairs = parse_pair.(input)
+
+  #Part 1
   straight((a, b)) = a[1] == b[1] || a[2] == b[2]
-  line((a, b)) = a < b ? (a:b) : (b:a)
-
-  straight_lines = map(line, filter(straight, indices))
-
-  #VolksWagen
+  straight_lines = line.(filter(straight, pairs))
   field = zeros(Int, 1000, 1000)
 
   foreach.(coord -> field[coord] += 1, straight_lines)
+  println(sum(field .≥ 2))
+
+  #Part 2
+  lines = line.(pairs)
+  field = zeros(Int, 1000, 1000)
+
+  foreach.(coord -> field[coord] += 1, lines)
   println(sum(field .≥ 2))
 end
